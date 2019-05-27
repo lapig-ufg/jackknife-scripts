a57d460def6242c661de7863e5404a8b62828e36
Maiky Nata <maikynata25@gmail.com>
2019-05-27 18:12:48 -0300

    bfasMonitor from CSV with for

---
+++ slurm_scritps/bfast01/run_bfastMonitor_from_CSV.R
@@ -0,0 +1,84 @@
+suppressMessages(library(parallel))
+suppressMessages(library(bfast))
+suppressMessages(library(raster))
+suppressMessages(library(dplyr))
+
+args = commandArgs(trailingOnly=TRUE)
+
+# Retrieve the ids of the coordinates in the csv file
+features <- read.csv("/data/SENTINEL/DPAT/BFASTMonitor/ids_seq_teste.csv", fill = TRUE, sep = ",")
+ndvi <- brick("/data/DADOS_GRID/pa_br_ndvi_maxmin_250_lapig_2000_2019.tif")
+
+select_seq_id <- features %>% select(lon, lat, seq_id) %>% filter(seq_id == args)
+# coordinatesRow <- select_seq_id %>% select(lon, lat)
+# coords_mat_s4 <- as.matrix(coordinatesRow)
+
+lat = select_seq_id$lat[1]
+lon = select_seq_id$lon
+
+# Creating a Coordinate S4 Class
+# setClass("coordinates", slots=list(lon="numeric", lat="numeric"))
+# s4_coordinates <- new("coordinates", lon = select_seq_id$lon, lat = select_seq_id$lat)
+
+
+bfast_apply = function(lon, lat) {
+	
+	run_bfast_monitor = function(ndvi_ts) {
+		
+		monitor <- bfastmonitor(ndvi_ts, start = c(2015, 12), formula = response ~ harmon + trend, history = "all")
+
+		monitor_breakpoint = monitor$breakpoint
+		monitor_magnitude = monitor$magnitude
+
+		return(data.frame( 
+				"monitor_breakpoint" = monitor_breakpoint,
+				"monitor_magnitude" = monitor_magnitude
+			))
+	}
+
+	cell <- cellFromXY(ndvi, c(lon, lat))
+	ndvi_vals <- as.numeric(ndvi[cell])
+	
+	# ndvi_vals <- ndvi_vals[1:414]
+
+	ndvi_ts <- ts(ndvi_vals, start= c(2000, 2), frequency = 23)
+
+	result_bfastmonitor <- run_bfast_monitor(ndvi_ts)
+
+	result = data.frame(
+		"lon" = lon,
+		"lat" = lat
+	)
+
+	bfastMonitorResult <- cbind(result, result_bfastmonitor)
+	bfastMonitorTableFormated <- write.table(bfastMonitorResult, col.names = FALSE, row.names = FALSE, sep = ";")
+}
+
+
+for (i in 1:length(lon)) {
+	bfast_apply(lon[i], lat)
+}
+
+
+# ncores = detectCores()
+# clusterPool = makeCluster(ncores)
+
+# hideOutput <- capture.output(output <- clusterEvalQ(clusterPool, {
+#   	suppressMessages(library(bfast))
+#   	suppressMessages(library(raster))
+# 	suppressMessages(library(rgdal))
+# }))
+
+# startTime <- Sys.time()
+# print("Running bfast")
+
+# bfastResultList <- parApply(cl = clusterPool, coords_mat_s4, 1, bfast_apply)
+# bfastResult <- do.call('rbind', bfastResultList)
+
+# endTime <- Sys.time()
+# cat("time:\n")
+# print((endTime - startTime))
+
+# bfastResultListTableFormated <- write.table(bfastResult, col.names = FALSE, row.names = FALSE, sep = ";")
+# print("Write CSV file")
+# write.table(bfastResult, col.names = FALSE, row.names = FALSE, sep = ";", file = paste0('layername',".csv"))
