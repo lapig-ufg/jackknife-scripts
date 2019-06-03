875a36cc3a8ff86ec32147fb6f11508f243af862
Maiky Nata <maikynata25@gmail.com>
2019-06-03 17:19:14 -0300

    sbatch Rscript from CSV

--- slurm_scritps/bfast01/sbatch_run_R_script_bfastMonitor_from_CSV.sh
+++ slurm_scritps/bfast01/sbatch_run_R_script_bfastMonitor_from_CSV.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #
 #SBATCH --job-name=run_bfast_monitor_from_shape.R
-#SBATCH --output=result-bfast-monitor-from-CSV-teste-700.csv
+#SBATCH --output=result-bfast-monitor-Pontos_Faltando.csv
 #SBATCH --partition=GRID-ALL
-#SBATCH --array=1-10
+#SBATCH --array=1-1953
 #SBATCH --open-mode=append
 #SBATCH --ntasks=1
 
-# Shape = 368919 coords -> array = 3690
+# CSV with 9699 points
+# 1953 faltando
 
 # Run bfast script with array args
 Rscript run_bfastMonitor_from_CSV.R $SLURM_ARRAY_TASK_ID
